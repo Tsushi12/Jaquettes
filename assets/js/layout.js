3aa71bfe1505351e4e2e6ab215dbe912f5e93297
Tsushi12 <elbouffi.driss@gmail.com>
2026-05-10 02:30:24 +0200

    10/05/2026

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -120,7 +120,7 @@ function isPhoneBrowser() {
 }
 
 const PHONE_LAYOUT_SESSION_KEY = "jaquettesPhoneLayoutMode";
-const DESKTOP_PHONE_VIEWPORT = "width=1200, initial-scale=1.0, viewport-fit=cover";
+const DESKTOP_PHONE_VIEWPORT = "width=device-width, initial-scale=0.1, minimum-scale=0.1, maximum-scale=5.0, viewport-fit=cover";
 const MOBILE_PHONE_VIEWPORT = "width=device-width, initial-scale=1.0, viewport-fit=cover";
 
 let stablePhoneLayoutMode = null;
